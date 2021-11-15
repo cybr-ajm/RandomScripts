c6c6e77a841b96944e6ef6e796e9b1b1e680cd63
Adam Markert <56974554+cybr-ajm@users.noreply.github.com>
2021-11-15 15:14:58 -0500

    Add files via upload

--- PublicDesktopACL.ps1
+++ PublicDesktopACL.ps1
@@ -1,10 +1,22 @@
-﻿$allUsersPath = 'C:\Users\Public\Desktop'
+﻿#Get Current ACL on All Users Desktop
+$allUsersPath = 'C:\Users\Public\Desktop'
 $acl = Get-Acl -Path $allUsersPath
 
+#Set Access Rules
 $identity = "NT AUTHORITY\INTERACTIVE"
-$fileSystemRights = "DeleteSubdirectoriesAndFiles"
+$fileSystemRights = "Read,Synchronize,DeleteSubdirectoriesAndFiles"
 $type = "Allow"
 $fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
 $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
 $acl.SetAccessRule($fileSystemAccessRule)
+
+#Set Audit Rules
+$AuditUser = "Everyone"
+$AuditRules = "Delete,DeleteSubdirectoriesAndFiles"
+$InheritType = "ContainerInherit,ObjectInherit"
+$AuditType = "Success"
+$auditRule = New-Object System.Security.AccessControl.FileSystemAuditRule($AuditUser,$AuditRules,$AuditType)
+$acl.SetAuditRule($auditRule)
+
+#Appy Changes
 Set-Acl -Path $allUsersPath -AclObject $acl
