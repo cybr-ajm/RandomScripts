4663036b045e4fef9cf768959e2d77721cf28bc6
Adam Markert <56974554+cybr-ajm@users.noreply.github.com>
2022-03-15 10:24:29 -0400

    Update EPM_ComputerOUTargetingViaRegistry.ps1

--- EPM_ComputerOUTargetingViaRegistry.ps1
+++ EPM_ComputerOUTargetingViaRegistry.ps1
@@ -8,14 +8,17 @@ if([string]::IsNullOrWhiteSpace($machineDN)){
 
     Write-EventLog -LogName "Application" -Source "CyberArk EPM" -EventID 1001 -EntryType Information -Message "EPM OU targeting condition not met, could not retrieve machine DN from registry" -Category 1 -RawData 10,20
     exit 1;
+    
 }elseif([string]::IsNullOrWhiteSpace($targetOU)){
 
     Write-EventLog -LogName "Application" -Source "CyberArk EPM" -EventID 1001 -EntryType Information -Message "EPM OU targeting condition not met, no target OU specified" -Category 1 -RawData 10,20
     exit 1;
+    
 }elseif($machineDN -notmatch $targetOU){
 
     Write-EventLog -LogName "Application" -Source "CyberArk EPM" -EventID 1001 -EntryType Information -Message "EPM OU targeting condition not met, $machineDN is not in $targetOU" -Category 1 -RawData 10,20
     exit 1;
+    
 }else{
 
     Write-EventLog -LogName "Application" -Source "CyberArk EPM" -EventID 1001 -EntryType Information -Message "EPM OU targeting condition is met, $machineDN is in $targetOU" -Category 1 -RawData 10,20
